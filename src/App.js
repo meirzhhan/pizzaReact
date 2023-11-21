3e3f258a385b1bc1e56b7f45aeac5a231d990ce3
meirzhhan <mbeymishev@mail.ru>
2023-11-22 03:34:36 +0600

    mockAPI(Симуляция Backend), Skeleton компонент

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Categories from './components/Categories';
 import Header from './components/Header';
-import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
-
-import pizzas from './assets/pizzas.json';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+import PizzaSkeleton from './components/PizzaBlock/PizzaSkeleton';
 
 import './scss/app.scss';
 
-console.log(pizzas);
-
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://655cfbb325b76d9884fe3e3a.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -23,9 +32,10 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {/* Создает массив и мапит на 10 скелетонов */}
+            {isLoading
+              ? [...new Array(8)].map((_, index) => <PizzaSkeleton key={index} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
---
+++ src/components/PizzaBlock/PizzaSkeleton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const PizzaSkeleton = () => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={500}
+    viewBox="0 0 280 500"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb">
+    <rect x="130" y="424" rx="30" ry="30" width="150" height="43" />
+    <rect x="0" y="440" rx="10" ry="10" width="91" height="27" />
+    <rect x="0" y="316" rx="10" ry="10" width="280" height="88" />
+    <rect x="0" y="269" rx="10" ry="10" width="280" height="27" />
+    <circle cx="140" cy="122" r="122" />
+  </ContentLoader>
+);
+
+export default PizzaSkeleton;
